3bfd08cf39b8722c03abb8b6efa5a1ce1b3a0545
omkar Pandey <omkar.pandey@brancosoft.com>
2025-07-08 14:44:38 +0530

    add APIs to set/update doctor availability, get availability, and fetch slots by date

---
+++ backend/routes/doctorAvailblityRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+const doctorAvailabilityRouter = express.Router();
+import doctorAvailabilityController from "../controller/doctorAvailblity.js"
+import { multerMiddleware } from "../middleware/multerMiddleware.js";
+import { isAuthenticated } from "../middleware/jwtAuth.js";
+
+// Route to add or update doctor availability
+doctorAvailabilityRouter.post(
+    "/add-or-update-availability",
+    multerMiddleware,
+    isAuthenticated,
+    doctorAvailabilityController.addOrUpdateAvailability
+);
+
+// Route to get doctor availability pass clinic_id for clinic visit or leave it empty for home visit
+doctorAvailabilityRouter.get(
+    "/get-availability",
+    isAuthenticated,
+    doctorAvailabilityController.getAvailabilityGroupedByDayWithSlots
+);
+
+// Route to get doctor availability by date, doctor id and clinic_id optional
+doctorAvailabilityRouter.get(
+    "/get-availability-by-date",
+    doctorAvailabilityController.getSlotsForDate
+);
+
+export default doctorAvailabilityRouter;
