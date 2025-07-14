4791426c455156b6a42946370e459691931b9f10
omkar Pandey <omkar.pandey@brancosoft.com>
2025-07-14 19:10:21 +0530

    CHANGES

--- backend/config/strip.js
+++ backend/config/strip.js
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 import * as DOTENV from '../utils/dotEnv.js';
 
 const stripe = new Stripe(DOTENV.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-07-01',
+  apiVersion: '2023-08-16',
 });
 
 export default stripe;
