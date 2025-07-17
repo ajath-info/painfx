11755df2ae383a56d0c0f172858661281ef83c1c
Vishesh Singh <vishesh.singh@ajath.com>
2025-07-17 12:03:18 +0530

    changes in the header section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,8 +7,26 @@ import DoctorSection from '../components/home/DoctorSection';
 import Features from '../components/home/Features';
 import Partner from '../components/home/Partner';
 import Blog from '../components/home/Blog';
-
+import {useState, useEffect} from 'react'
+import { useNavigate} from 'react-router-dom';
 const Home = () => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  useEffect(()=>{
+    const token = localStorage.getItem('token');
+    const user = JSON.parse(localStorage.getItem('user'));
+    console.log(user)
+    if(token && user){
+      setIsLoggedIn(true);
+      if(user.role === 'doctor'){
+      navigate('/doctor/dashboard')
+    }else if(user.role === 'patient'){
+      navigate('/patient/dashboard')
+    }
+    }
+   
+  },[navigate]);
+  
   return (
     <PublicLayout>
       <HeroBanner />
